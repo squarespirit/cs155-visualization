ee9d292fa139a6723bd683b699f1d38080ebc7cf
Matthew Wu <mewu@caltech.edu>
2017-03-08 10:48:15 -0800

    Data reader for movies.txt
    Fix encoding issue for data/data.txt. Have issues for encoding for movies.txt.

--- data_readers.py
+++ data_readers.py
@@ -1,5 +1,7 @@
 # For stuff that reads data from files in data/
 
+import io
+
 
 def read_data(filename='data/data.txt'):
     """Read file of (user, movie, rating) data.
@@ -9,7 +11,7 @@ def read_data(filename='data/data.txt'):
     """
     Y = []
 
-    with open(filename, 'r') as f:
+    with io.open(filename, 'r', encoding='utf8') as f:
         for line in f:
             rating_strs = line.strip().split('\t')
             rating = list(map(int, rating_strs))
@@ -22,3 +24,27 @@ def read_data(filename='data/data.txt'):
             Y.append(rating)
 
     return Y
+
+
+def read_movies(filename='data/movies.txt'):
+    """Read file of (movie_id, movie_title, genres...) data.
+    :return movie_titles, genres.
+        titles: titles[i] is the title of movie with id i.
+        genres: genres[i] is the one-hot genre vector for the movie with id i.
+        IMPORTANT: movie ids are 0-indexed.
+    """
+    titles = []
+    genres = []
+
+    with io.open(filename, 'r', encoding='utf8') as f:
+        for line in f:
+            strings = line.strip().split('\t')
+            titles.append(strings[1])
+            genres.append(
+                [int(genre_str) for genre_str in strings[2:]]
+            )
+
+    return titles, genres
+
+
+print read_movies()
